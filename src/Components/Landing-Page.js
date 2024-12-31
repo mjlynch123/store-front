518945f41e2629726dfc6209aeb60ef588daed69
MJ lynch <mjlynch35@yahoo.com>
2024-12-31 16:34:36 -0600

    Added header and buttons

--- src/Components/Landing-Page.js
+++ src/Components/Landing-Page.js
@@ -4,7 +4,14 @@ export default function LandingPage() {
     return (
         <>
             <div className='main'>
-                <h1>Welcome to Cloud Nine Dispensary</h1>
+                <Header>
+                    <h1>Welcome to Cloud Nine Dispensary</h1>
+                    <p>Our mission is to provide the best quality cannabis products to our customers.</p>
+                    <div className="buttons">
+                        <button>Shop Now</button>
+                        <button>Learn More</button>
+                    </div>
+                </Header>
             </div>
         </>
     )
