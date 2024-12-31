f800c3c5948496cdc00ad590c60b264c157529c0
MJ lynch <mjlynch35@yahoo.com>
2024-12-31 16:20:17 -0600

    Added title to the landing page

--- src/Components/Landing-Page.js
+++ src/Components/Landing-Page.js
@@ -3,7 +3,9 @@ import react, { useState, useEffect } from 'react';
 export default function LandingPage() {
     return (
         <>
-
+            <div className='main'>
+                <h1>Welcome to Cloud Nine Dispensary</h1>
+            </div>
         </>
     )
 }
