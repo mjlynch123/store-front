71535727dd8430d57edf5b596c1b985197275cda
MJ lynch <mjlynch35@yahoo.com>
2024-12-31 16:40:38 -0600

    Displayed landing page to website at route /

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './Components/Landing-Page';
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
